ad0ddb4463ff976c77b83bffa39d567f7fcdff95
Mahmoud5839 <hooda7113@gmail.com>
2026-05-05 01:15:14 +0300

    index.js for cors commit

--- api/index.js
+++ api/index.js
@@ -47,6 +47,16 @@ app.use(async (req, res, next) => {
     next();
 });
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 // routes
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
